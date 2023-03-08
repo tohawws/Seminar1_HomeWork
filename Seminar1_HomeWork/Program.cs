1a46acac625c77c979f897938af26176754fe108
Anton Korotkov <tohawws777@gmail.com>
2023-03-08 21:00:15 +0600

    Corrected the code for the first job

--- Seminar1_HomeWork/Program.cs
+++ Seminar1_HomeWork/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает 
 //два числа и выдает, какое число большее, а какое меньшее.
 
-/* 
+/*  
 Console.WriteLine("Введите число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число");
@@ -9,14 +9,16 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 if (number1 > number2)
 {
-  Console.Write("Большее число = " + number1);
+  Console.WriteLine("Большее число = " + number1);
+  Console.WriteLine("Меньшее число = " + number2);
 }
 
 else
 {
-  Console.Write("Большее число = " + number2);
+  Console.WriteLine("Большее число = " + number2);
+  Console.WriteLine("Меньшее число = " + number1);
 } 
-/*
+*/
 
 
 
