39ebedb610492b88a8c108102dbb35744b78e433
Anton Korotkov <tohawws777@gmail.com>
2023-03-08 20:46:32 +0600

    initial commit

---
+++ Seminar1_HomeWork/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдает, какое число большее, а какое меньшее.
+
+/* 
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+  Console.Write("Большее число = " + number1);
+}
+
+else
+{
+  Console.Write("Большее число = " + number2);
+} 
+/*
+
+
+
+
+/* Задача 4: Напишите программу, которая принимает на вход 
+три числа и выдает максимальное из этих чисел. */
+
+/*  
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+  Console.Write("Максимальное число = " + number1);
+}
+
+else if (number2 > number3)
+{
+  Console.Write("Максимальное число = " + number2);
+}
+
+else if (number3 > number1)
+{
+  Console.Write("Максимальное число = " + number3);
+}
+*/
+
+
+
+/* Задача 6: Напишите программу, которая на вход принимает число и выдает, 
+является ли число четным (делится ли оно на два без остатка) */
+
+/* 
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 %2 == 0)
+{
+  Console.Write("Чётное число = " + number1);
+}
+else
+{
+  Console.Write("Нечётное число = " + number1);
+}
+*/
+
+
+
+/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. */
+
+/* 
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i < N)
+{
+  Console.WriteLine(i + 1);
+  i = i + 2;
+}
+ */
